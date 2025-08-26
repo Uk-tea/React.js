977fd7e811698a8748fabdfed211ffb6d637dcb1
usamakhan <usamakhan@digitalgravity.ae>
2025-08-26 18:43:36 +0500

    hook update

--- slider/sliderani/src/App.jsx
+++ slider/sliderani/src/App.jsx
@@ -1,34 +1,68 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  let [max, setMax] = useState(20);
+  let [min, setMin] = useState(0);
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    if(counter < max){
+      setCounter(counter + 1)
+    }
+  }
+  const removeValue = () => {
+    if(counter > min){
+      setCounter(counter - 1)
+    }
+  }
+
+  const resetValue = () => setCounter(min)
+
+
+  // Dynamic color chnages
+
+let color = "green" 
+if(counter >= max){
+  color = "red";
+}else if (counter >= max - Math.ceil((max - min) * 0.4)) {
+    // agar counter last 30% range me ho
+    color = "orange";
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="w-full flex justify-center items-center flex-col gap-6 text-xl font-bold">
+      <h1 style={{color}}>React Counter : {counter}</h1>
+      <button onClick={addValue}
+       disabled={ counter >= max }>Add Value :
+        {counter}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button onClick={removeValue}
+       disabled={ counter <= min }>
+        Remove Value : {counter}
+        </button>
+      <button onClick={resetValue}>
+        Reset Value
+        </button>
+        <div className="flex gap-4 justify-center items-center">
+          setMin
+          <input 
+          type="Number"
+          value={min}
+          placeholder='Set Min Value' 
+          onChange={(e) => setMin(Number(e.target.value))}
+          className="border p-2 rounded-2xl"
+          />
+          setMax
+          <input type="Number"
+          value={max}
+          placeholder='Set Max Value' 
+          onChange={(e) => setMax(Number(e.target.value))}
+          className="border p-2 rounded-2xl"
+           />
+        </div>
+    </div>
   )
 }
 
