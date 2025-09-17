a43dc3e6a2c29822b1b85d8e88fbf784789003f2
usamakhan <usamakhan@digitalgravity.ae>
2025-09-17 16:34:12 +0500

    updated

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -116,7 +116,7 @@ const App = () => {
                     </button>
                     <button
                       onClick={() => deleteTodo(index)}
-                      className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+                      className="bg-red-800 text-white px-3 py-1 rounded hover:bg-red-600"
                     >
                       Delete
                     </button>
